249138e1f103659fc5515d73c188fe43d5b869b5
Randall Ishino <rishino24@gmail.com>
2017-12-12 23:18:02 -0800

    final stage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,16 @@ var words = [
     "tennis",
     "hockey",
     "cycling",
-    "racing"
+    "racing",
+    "boxing",
+    "surfing"
 ];
 
 // variables for amount of wins, guesses, and empty arrays for storing wrong letters and hidden word.
 var wins = 0;
+var losses = 0;
 var guesses = 10;
-var answerWord = [];
+var underScores = [];
 var wrongLetter = [];
 var randomWord = '';
 
@@ -27,69 +30,85 @@ var hangmanWord = document.getElementById("hangman-words");
 var numberOfGuesses = document.getElementById("guesses");
 var numberOfWins = document.getElementById('wins');
 var lettersUsed = document.getElementById("letters-used");
+var numberOfLosses = document.getElementById("losses");
 
 // starts game
 function launchGame() {
 // resets variables to global value
-var answerWord = [];
-var wrongLetter = [];
-var guesses = 10;
+guesses = 10;
 
 // generates random word from array
 randomWord = words[Math.floor(Math.random() * words.length)];
+// splitting randomWord into an array with separate strings
 splitWord = randomWord.split('');
 console.log(randomWord);
-
+// resets to empty array
+underScores = [];
+wrongLetter = [];
 // hidden answer array with blank underscores
 for (var i = 0;  i < splitWord.length; i++) {
-    answerWord.push(' _ ');
+    underScores.push(' _ ');
 }
-hangmanWord.innerHTML = answerWord.join('');
+hangmanWord.innerHTML = underScores.join('');
 
 };
 
-// registers user key
+// registers user key and calls both our  letterCheck function
+// and checkWinOrLoss function
 document.onkeyup = function(event) {    
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     letterCheck(userGuess);
+    checkWinOrLoss(userGuess);
 }
 
 // checks for right or wrong letters
 function letterCheck (userGuess) {
+    // looping through the array and if the letter IS found
     if (randomWord.indexOf(userGuess) > -1) {
-        console.log(true);
         for( var k = 0; k < splitWord.length; k++) {
             if (splitWord[k] === userGuess){
-                answerWord[k] = userGuess;
-                hangmanWord.innerHTML = answerWord.join('');                 
+                underScores[k] = userGuess;
+                hangmanWord.innerHTML = underScores.join('');                 
             }
         }
     }
-
-    if (answerWord.join('') == splitWord.join('')) { 
+}
+// checking the result of game and adjusting wins/losses
+function checkWinOrLoss(userGuess) {
+    // if both words are matched, adjust win counter, write to html
+    // and call our launchGame function
+    if (underScores.join('') == splitWord.join('')) { 
     wins++;
     numberOfWins.innerHTML = "Number of wins: " + wins;
-    alert("You win!");
+    underScores = []; 
+    guesses = 10;   
+    alert("Good job! You won!");
     launchGame();
-    }
+    } // if word is NOT in array then do this
     else if (randomWord.indexOf(userGuess) === -1) {
         guesses--;
+        wrongLetter.push(userGuess);
+        lettersUsed.innerHTML = "Letters used: " + wrongLetter.join('');
         numberOfGuesses.innerHTML = "Guesses remaining: " + guesses;
+        console.log(wrongLetter);
     }
-
+    // if amount of guesses equal 0, update losses and display to html
     if (guesses == 0){
-        alert("You lost! Click reset to play again");
+    losses++;
+    alert("Sorry you lost!" + " the answer was "+ randomWord);
+    numberOfLosses.innerHTML = "Number of losses: " + losses;
+    launchGame();
+
     }
 }
-
-  
 
 
 launchGame();
 
 
 
+
     // reset game 
     document.getElementById("reset").onclick = function() {
-        location.reload();
+        launchGame();
     };
